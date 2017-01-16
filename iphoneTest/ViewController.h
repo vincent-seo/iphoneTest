ed73546e5f4b1e67f62e7b6607edf985d84fe273
vincent.seo <xunaoo@gmail.com>
2017-01-16 12:49:13 +0800

    Initial Commit

---
+++ iphoneTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iphoneTest
+//
+//  Created by MANSHENG XU on 1/16/17.
+//  Copyright © 2017 MANSHENG XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iphoneTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iphoneTest
+//
+//  Created by MANSHENG XU on 1/16/17.
+//  Copyright © 2017 MANSHENG XU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
